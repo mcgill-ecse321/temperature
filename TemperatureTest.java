4126cf5ee5daab41e1a25621e48ec92d0898cc65
Aditya Mahajan <adityam@umich.edu>
2013-10-06 20:15:21 -0400

    Skeleton code for unit testing

---
+++ TemperatureTest.java
@@ -0,0 +1,24 @@
+/** 
+ * @author Aditya Mahajan <aditya.mahajan@mcgill.ca>
+ * @version 2013.10.06
+ * Unit Testing Temperature class
+ */
+
+import org.junit.* ;
+import static org.junit.Assert.* ;
+
+public class TemperatureTest {
+
+  // Add multiple tests to check all functions of
+  // {@Code Temperature} class.
+
+  // @Test
+  // public void .... (){
+  //    ...
+  // }
+
+  // @Test
+  // public void .... (){
+  //    ...
+  // }
+}
